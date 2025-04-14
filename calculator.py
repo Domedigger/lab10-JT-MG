cba46b60f8e0679f2b1706d82a188425808ad267
Jordon <jordontaylorjpt@gmail.com>
2025-04-14 16:04:39 -0400

    modified calcculator p1

--- calculator.py
+++ calculator.py
@@ -13,15 +13,15 @@ def subtract(a, b): return a - b
 
 def multiply(a, b): return a * b
 
-def logarithm(a, b):
-    if b <= 0 or a <= 0 or a == 1:
-        raise ValueError
-    return math.log(b, a)
-
 def divide(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
+
+def logarithm(a, b):
+    if b <= 0 or a <= 0 or a == 1:
+        raise ValueError
+    return math.log(b, a)
 
 def exponent(a, b): return a ** b
 
