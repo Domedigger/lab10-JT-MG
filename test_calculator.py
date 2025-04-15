35bb3d2bfebdfb036909f97c236dbece494b38a5
Morgan Granade <morgranade@gmail.com>
2025-04-14 20:08:50 -0400

    le finale

--- test_calculator.py
+++ test_calculator.py
@@ -14,7 +14,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 4), 6)
     # def test_subtract(self): # 3 assertions
     #     fill in code
-    def test_sub(self):
+    def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(7, 2), 5)
         self.assertEqual(subtract(9, 3), 6)
